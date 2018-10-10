f294c02a383d68390e9c75003dd3ab2849129ce2
crazydf25 <624056275@qq.com>
2018-10-10 21:29:22 +0200

    Add files via upload

---
+++ bme280_bluetooth1.ino
@@ -0,0 +1,24 @@
+#include "Seeed_BME280.h"
+#include <Wire.h>
+#include <SoftwareSerial.h>
+SoftwareSerial serial2(6,7);
+BME280 bme280;
+
+void setup()
+{
+  Serial.begin(115200);
+  serial2.begin(115200);
+  if(!bme280.init()){
+    Serial.println("Device error!");
+  }
+}
+
+void loop(){
+  int a,b,c;
+    a  = abs(bme280.getPressure());
+    b = bme280.getTemperature();
+    c = bme280.getHumidity()*3;
+    int d = map(a,27500,32000,0,255);
+    serial2.write(d);
+    Serial.println(a);
+}
---
+++ wire_transmission.ino
@@ -0,0 +1,33 @@
+char dataID1 = 'A';
+char dataID2 = 'B';
+//#include <SoftwareSerial.h>
+
+//Tx to 6, Rx to 7
+//Tx to 9, Rx to 10
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+}
+
+void loop() {
+  if(Serial.available()) {
+    int sensorValue1 = Serial.read();
+    Serial.print('A');
+    Serial.println(sensorValue1);
+//    sendDataToProcessing(dataID1, sensorValue1);
+  }
+  if(Serial1.available()) {
+    int sensorValue2 = Serial1.read();
+    Serial1.print('B');
+    Serial1.println(sensorValue2);
+//    sendDataToProcessing(dataID2, sensorValue2);
+  }
+  
+  delay(2);
+}
+
+//void sendDataToProcessing(char symbol, float data){
+//  Serial.print(symbol);
+//  Serial.println(data);
+//}
